a2620a8845eaf228c2e458463e89f29d45c23663
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-03-09 15:11:47 +0300

    🔥 Java Classpath ve JAR Bağımlılıkları Ayarlandı! 🚀
    Sonunda classpath sorununu çözdüm! 📂🚀 java -classpath komutuyla lib klasöründeki tüm bağımlılıkları ve geçerli dizini doğru şekilde bağladım. 💻 Artık HelloWorld sınıfını çalıştırırken eksik kütüphane hatası almayacağım! 😎 Böylece her şey düzgün çalışacak, bağımlılıklar doğru şekilde yüklenecek! ✅

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Ben: Selamun Aleyküm Dünya");
+		System.out.println("Dünya: A.S. dayııı");
+	}
+}
---
+++ jar2/HelloWorld.java
@@ -0,0 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Selamun Aleykum");
+        System.out.println(StringUtils.capitalize("aleykum selam"));
+    }
+}
