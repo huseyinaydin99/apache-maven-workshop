84991a10bcca0f6851195226533728405950a436
huseyinaydin99 <huseyinaydin99@gmail.com>
2025-03-13 17:00:11 +0300

    Maven Archetypes Kullanarak Yeni Proje Şablonu Oluşturma 🎉📦
    Maven Archetypes ile yeni bir proje şablonu oluşturma sürecine başladım! 💻🛠️ Bu işlem, belirli bir arketipe dayalı olarak hızlıca yeni bir projeyi kurmamızı sağlıyor. Özellikle maven-archetype-simple arketipini kullanarak basit bir Java projesi başlattım. Ancak dikkat edilmesi gereken nokta, doğru arketip ve versiyonun seçilmesi gerektiği. 🧐🔍 Eğer yanlış yazım veya uyumsuz versiyon seçimi yapılırsa, proje kurulumu hatalı olabilir! 🚫 Bu commit'te, Maven komutlarıyla ilgili temel adımları atarak proje şablonunu oluşturmayı hedefledim. 🎯

---
+++ 026_Archetypes_Project/TestProject/src/main/java/tr/com/huseyinaydin/App.java
@@ -0,0 +1,13 @@
+package tr.com.huseyinaydin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
